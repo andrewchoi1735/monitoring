cb3934b58695ab3ad18f6d5f80d0cc789e2a9faf
andrewchoi1735 <doyun4199@gmail.com>
2025-01-09 11:43:07 +0900

    web_status maaping

--- Othetak/main.py
+++ Othetak/main.py
@@ -8,10 +8,22 @@ def check_website_status(url):
 		response = requests.get(url)
 		if response.status_code == 200:
 			print("웹페이지가 정상적으로 작동하고 있습니다.")
+			status = 1  # 정상 상태
 		else:
 			print("웹페이지에 문제가 있습니다. 상태 코드:", response.status_code)
+			status = 0  # 문제 발생 상태
 	except requests.exceptions.RequestException as e:
 		print("웹페이지에 문제가 있습니다. 에러 메시지:", e)
+		status = 0  # 문제 발생 상태
+
+	push_to_prometheus(status)
+
+
+def push_to_prometheus(status):
+	url = "http://localhost:9091/metrics/job/website_status"
+	data = f"website_status {status}\n"
+	response = requests.post(url, data=data)
+	return response.status_code
 
 
 if __name__ == "__main__":
